65deff68f97eff4929dfb2928571d995e0e70b0b
Aishwary Patle <61194375+Aishwarypatle@users.noreply.github.com>
2023-01-24 00:47:50 +0530

    update

--- myapp/src/components/Slider.jsx
+++ myapp/src/components/Slider.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { ArrowLeftOutlined,ArrowRightOutlined } from '@mui/icons-material'
 import styled from 'styled-components'
-import Pic from "../images/business.jpg"
+import sliderData from "../data"
+
+
 const Container = styled.div`
     display: flex;
     height: 100vh;
     width: 100%;
+    overflow: hidden;
     
 `
 
@@ -25,16 +28,20 @@ const Arrow = styled.div`
     margin: auto;
     cursor: pointer;
     opacity: 0.5;
+    z-index: 2;
 `
 
 const Wrapper = styled.div`
     height: 100%;
+    transform: translateX(0vw);
+    display: flex;
 `
 const Slide = styled.div`
     height: 100vh;
     width: 100vw;
     display: flex;
     align-items: center;    
+    background-color: #${props=>props.bg};
 `
 const ImageContainer = styled.div`
     flex: 1;
@@ -48,26 +55,52 @@ const InfoContainer = styled.div`
     padding: 50px;
 `
 const Title  = styled.h1`
-    
+    font-size: 70px;
+`
+
+const  Desc  = styled.p`
+    margin: 50px;
+    font-size: 20px;
+    font-weight: 500;
+    letter-spacing: 4px;
+`
+
+const Button  = styled.button`
+    padding: 12px;
+    font-size: 18px;
+    background-color: transparent;
+    cursor: pointer;
+
 `
 
 
 const Slider = () => {
+    const handleClick = () =>
+    {
+
+    }
+
   return (
     <Container>
-        <Arrow direction="left">
+        <Arrow direction="left"  onClick={()=> handleClick('left')}>
             <ArrowLeftOutlined/>
         </Arrow>
+        {
+            sliderData.map((item)=>(
             <Wrapper>
-                <Slide>
+                <Slide bg={item.bg}>
                     <ImageContainer>
-                       <img style={{height:'80%'}} src={Pic} alt='im'></img>
+                       <img style={{height:'80%'}} src={item.image} alt='im'></img>
                     </ImageContainer>
-                    <InfoContainer></InfoContainer>
+                    <InfoContainer>
+                        <Title>{item.title}</Title>
+                        <Desc>{item.description}</Desc>
+                        <Button>{item.}</Button>
+                    </InfoContainer>
                 </Slide>
             </Wrapper>
-
-        <Arrow direction="right">
+        ))}
+        <Arrow direction="right" onClick={()=> handleClick('right')}>
             <ArrowRightOutlined/>
         </Arrow>
     </Container>
