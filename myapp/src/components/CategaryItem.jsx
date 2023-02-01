c38df02372cf085b7325e748233074f1305fe6b0
Aishwary Patle <61194375+Aishwarypatle@users.noreply.github.com>
2023-02-01 17:34:59 +0530

    categaries completed

--- myapp/src/components/CategaryItem.jsx
+++ myapp/src/components/CategaryItem.jsx
@@ -2,23 +2,41 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Container = styled.div`
-  
+  flex: 1;
   margin: 3px;
-  flex-wrap: wrap;
-  width: 100%;
-  overflow: hidden;
+  height: 70vh;
+  position: relative;
 `
-const Title  = styled.h1``
+const Title  = styled.h1`
+ color:white;
+    margin-bottom: 20px;`
 
 const Button = styled.button`
-  padding: 12px;
-  font-size: 18px;
-  background-color: transparent;
-  cursor: pointer;
+    border:none;
+    padding: 10px;
+    background-color: white;
+    color:gray;
+    cursor: pointer;
+    font-weight: 600;
 `
 const Info = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
 `
-const Image = styled.img``
+const Image = styled.img`
+   width: 100%;
+  height: 100%;
+  object-fit: cover;
+  
+  `
 
 
 
