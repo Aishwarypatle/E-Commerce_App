6c217c0a86c86907b001a96cd46add1aa7222c66
Aishwary Patle <61194375+Aishwarypatle@users.noreply.github.com>
2023-02-01 17:58:12 +0530

    products & product component added

---
+++ myapp/src/components/Newsletter.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+
+`
+
+const Newsletter = () => {
+  return (
+    <Container>
+        <Title></Title>
+        
+    </Container>
+  )
+
+export default Newsletter
